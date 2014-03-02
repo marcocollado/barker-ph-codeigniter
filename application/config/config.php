a3c7cd11ddfef055988272eb85555292a2ceea2a
marcocollado <marco.collado@server.fake>
2014-03-02 15:46:11 -0800

    basep

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://barker-ph.azurewebsites.net';
+$config['base_url']	= 'http://barker-ph.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
